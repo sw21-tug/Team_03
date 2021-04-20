b4dfa44eb779da702be323a59d229ab3de68deb2
Lorenz Kutschka <lorenz.kutschka@student.tugraz.at>
2021-04-20 23:18:15 +0200

    FE-1 advanced register gui [KL,SM]

--- Frontend/Android/app/src/main/java/com/team03/cocktailrecipesapp/MainActivity.kt
+++ Frontend/Android/app/src/main/java/com/team03/cocktailrecipesapp/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.team03.cocktailrecipesapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+    }
+    fun onClickBtnRegister(view: View){
+        val btnRegister: Button = findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- Frontend/Android/app/src/main/java/com/team03/cocktailrecipesapp/RegisterActivity.kt
+++ Frontend/Android/app/src/main/java/com/team03/cocktailrecipesapp/RegisterActivity.kt
@@ -1,11 +1,68 @@
 package com.team03.cocktailrecipesapp
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class RegisterActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
+
+
     }
+
+    fun login(): Boolean {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        return true
+    }
+
+    fun validateInput(): Boolean {
+        val etUsername: EditText = findViewById(R.id.txt_username);
+        val etPassword: EditText = findViewById(R.id.txt_password);
+        val etPasswordRepeated: EditText = findViewById(R.id.txt_password_repeat);
+        var ret_val: Boolean
+        ret_val = true;
+        if (etUsername.text.toString() == "") {
+            etUsername.highlightColor = Color.RED;
+            ret_val = false
+        }
+        if (etPassword.text.toString() == "") {
+            etPassword.highlightColor = Color.RED;
+            ret_val =  false
+        }
+        if (etPasswordRepeated.text.toString() == "") {
+            etPasswordRepeated.highlightColor = Color.RED;
+            ret_val = false
+        }
+        if (etPasswordRepeated.text.toString() != etPassword.text.toString()) {
+            etPasswordRepeated.error = ""
+            ret_val = false;
+        }
+
+        if (etPassword.text.toString() == "") {
+            etPassword.error = "Please Enter Password"
+            ret_val = false
+        }
+
+
+        return ret_val
+    }
+
+    fun registerOnClick(view: View){
+        val btnRegister: Button = findViewById(R.id.btn_register);
+        btnRegister.setOnClickListener {
+            if (validateInput()) {
+                login();
+            }
+        }
+    }
+
+
 }
