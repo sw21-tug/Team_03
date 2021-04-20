5dbdad044f21dd72e57545857ab31e378e4b17eb
bernhard schnoegl <bernhard.schnoegl@student.tugraz.at>
2021-04-20 22:51:03 +0200

    CRA-002 login form [BS,DS]

--- Frontend/Android/app/src/androidTest/java/com/team03/cocktailrecipesapp/ExampleInstrumentedTest.kt
+++ Frontend/Android/app/src/androidTest/java/com/team03/cocktailrecipesapp/ExampleInstrumentedTest.kt
@@ -8,6 +8,15 @@ import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 
+
+
+import com.team03.cocktailrecipesapp.ui.login.LoginViewModel
+import com.team03.cocktailrecipesapp.ui.login.LoginActivity
+
+
+
+//"../../com.team03.cocktailrecipesapp/ui.login/LoginViewModel"
+
 /**
  * Instrumented test, which will execute on an Android device.
  *
@@ -16,9 +25,35 @@ import org.junit.Assert.*
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
-    fun useAppContext() {
+
+    fun isPasswordValid(password: String): Boolean {
+        return password.length >= 5
+    }
+
+    fun useAppContext()
+    {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.team03.cocktailrecipesapp", appContext.packageName)
+
+
+        val password:String = "12345678";
+        val valid = isPasswordValid(password)
+        assertEquals(true, LoginViewModel::isPasswordValid(password))
+
+    fun password()
+    {
+        (LoginViewModel(loginRepository = password))(password)
+
+
+       // onView(withId(R.id.password)).perform(click())
     }
+
+
+
+    }
+
+
+
+
 }
--- Frontend/Android/app/src/main/java/com/team03/cocktailrecipesapp/MainActivity.kt
+++ Frontend/Android/app/src/main/java/com/team03/cocktailrecipesapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.team03.cocktailrecipesapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.team03.cocktailrecipesapp.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+    }
+
+    fun loginOnClick(view: View){
+        val button: Button = findViewById(R.id.button_login)
+        button.setOnClickListener {
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
