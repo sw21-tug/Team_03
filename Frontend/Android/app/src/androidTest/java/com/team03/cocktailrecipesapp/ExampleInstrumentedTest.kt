76628370de65ef8a7e3729ad979a78be36c9668c
David-Sebastian Schrey <d.schrey@student.tugraz.at>
2021-04-20 22:24:40 +0200

    CRA-002 login tests [BS,DS]

--- Frontend/Android/app/src/androidTest/java/com/team03/cocktailrecipesapp/ExampleInstrumentedTest.kt
+++ Frontend/Android/app/src/androidTest/java/com/team03/cocktailrecipesapp/ExampleInstrumentedTest.kt
@@ -1,5 +1,11 @@
 package com.team03.cocktailrecipesapp
 
+import android.widget.EditText
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +13,8 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +23,29 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.team03.cocktailrecipesapp", appContext.packageName)
+    }
+
+    @Test
+    fun loginDataTest(){
+        onView(withId(R.id.username)).perform(typeText("Testusername"), closeSoftKeyboard())
+        onView(withId(R.id.password)).perform(typeText("Testpassword"), closeSoftKeyboard())
+    }
+
+    @Test
+    fun loginButton() {
+        onView(withId(R.id.loginbtn)).perform(click())
+    }
+
+    @Test
+    fun registerButton() {
+        onView(withId(R.id.registerbtn)).perform(click())
     }
 }
--- Frontend/Android/app/src/main/java/com/team03/cocktailrecipesapp/MainActivity.kt
+++ Frontend/Android/app/src/main/java/com/team03/cocktailrecipesapp/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.team03.cocktailrecipesapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun loginOnClick(view: View){
+
     }
 }
