cdf04ae2fa1b62fb992409d32841e91b72420f1b
michael_schaffer <michael.schaffe@student.tugraz.at>
2021-04-21 11:57:14 +0200

    FE-4 Implemented basic connection [SM,SM]

--- Frontend/Android/app/src/main/java/com/team03/cocktailrecipesapp/serverAPI.kt
+++ Frontend/Android/app/src/main/java/com/team03/cocktailrecipesapp/serverAPI.kt
@@ -1,10 +1,19 @@
 package com.team03.cocktailrecipesapp
 
-import com.android.volley.toolbox.StringRequest
+import android.content.Context
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.HttpResponse
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.JsonRequest
 import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
-class serverAPI
+class serverAPI(context: Context)
 {
+    val context_ = context
+
     fun register(username: String, password_hash: String) : String
     {
         val register_json = """
@@ -12,9 +21,10 @@ class serverAPI
                 "name": """" + username + """",
                 "password_hash": """" + password_hash + """"
             }
-            """;
+            """
 
-        return sendAndReceive(register_json);
+        val json = JSONObject(register_json)
+        return sendRequest(json).toString()
     }
 
     fun login(username: String, password_hash: String) : String
@@ -24,9 +34,10 @@ class serverAPI
                 "name": """" + username + """",
                 "password_hash": """" + password_hash + """"
             }
-            """;
+            """
 
-        return sendAndReceive(login_json);
+        val json = JSONObject(login_json)
+        return sendRequest(json).toString()
     }
 
     fun addRecipe(user_id: Int, name: String, preptime_minutes: Int, difficulty: Int, instruction: String, ingredient_names: List<String>) : String
@@ -39,7 +50,7 @@ class serverAPI
                 "difficulty": """" + difficulty.toString() + """",
                 "instruction": """" + instruction + """",
                 "ingredient_names": [
-            """;
+            """
 
         for (i in ingredient_names)
         {
@@ -53,7 +64,8 @@ class serverAPI
             }
         }
 
-        return sendAndReceive(add_recipe_json);
+        val json = JSONObject(add_recipe_json)
+        return sendRequest(json).toString()
     }
 
     fun rateRecipe(user_id: Int, recipe_id: Int, value: Int) : String
@@ -64,19 +76,16 @@ class serverAPI
                 "recipe_id": """" + recipe_id.toString() + """",
                 "value": """" + value.toString() + """"
             }
-            """;
+            """
 
-        return sendAndReceive(rate_recipe_json);
+        val json = JSONObject(rate_recipe_json)
+        return sendRequest(json).toString()
     }
 
     fun getRecipes() : String
     {
-        val get_recipe_json = """
-            {
-            }
-            """;
-
-        return sendAndReceive(get_recipe_json);
+        val json = JSONObject()
+        return sendRequest(json).toString()
     }
 
     fun changePassword(user_id: Int, old_password_hash: String, new_password_hash: String) : String
@@ -87,9 +96,10 @@ class serverAPI
                 "old_password_hash": """" + old_password_hash + """",
                 "new_password_hash": """" + new_password_hash + """"
             }
-            """;
+            """
 
-        return sendAndReceive(change_password_json);
+        val json = JSONObject(change_password_json)
+        return sendRequest(json).toString()
     }
 
     fun deleteRecipe(user_id: Int, recipe_id: Int) : String
@@ -99,9 +109,10 @@ class serverAPI
                 "user_id": """" + user_id.toString() + """",
                 "recipe_id": """" + recipe_id.toString() + """"
             }
-            """;
+            """
 
-        return sendAndReceive(delete_recipe_json);
+        val json = JSONObject(delete_recipe_json)
+        return sendRequest(json).toString()
     }
 
     fun likeRecipe(user_id: Int, recipe_id: Int) : String
@@ -111,40 +122,28 @@ class serverAPI
                 "user_id": """" + user_id.toString() + """",
                 "recipe_id": """" + recipe_id.toString() + """"
             }
-            """;
+            """
 
-        return sendAndReceive(like_recipe_json);
+        val json = JSONObject(like_recipe_json)
+        return sendRequest(json).toString()
     }
 
-    private fun sendAndReceive(buffer: String) : String
+    private fun sendRequest(jsonObject: JSONObject) : JSONObject
     {
-        /*
-        // Instantiate the RequestQueue.
-        val queue = Volley.newRequestQueue(this)
-        val url = "localhost:5000"
+        val queue = Volley.newRequestQueue(context_)
+        val url = "localhost:3000"
 
-        // Request a string response from the provided URL.
-        val stringRequest = StringRequest(Request.Method.GET, url,
-                Response.Listener<String> { response ->
-                    // Display the first 500 characters of the response string.
-                    textView.text = "Response is: ${response.substring(0, 500)}"
-                },
-                Response.ErrorListener { textView.text = "That didn't work!" })
+        var response = JSONObject()
 
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest)
-*/
-        send(buffer);
-        return receive();
-    }
+        val request = JsonObjectRequest(url, jsonObject, {
+            response = it
+        }, {
+            print("Error: No connection possible\n")
+        })
 
-    private fun send(buffer: String)
-    {
+        queue.add(request)
+        queue.start()
 
-    }
-
-    private fun receive() : String
-    {
-        return "";
+        return response
     }
 }
