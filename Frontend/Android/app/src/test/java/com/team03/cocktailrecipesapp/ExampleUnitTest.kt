8aee6558518cd3f9cdf63335d865e10b924716c0
Manuel Schoellauf <manuel.schoellauf@student.tugraz.at>
2021-04-21 10:47:53 +0200

    FE-4 creating communication class [SM,SM]

--- Frontend/Android/app/src/test/java/com/team03/cocktailrecipesapp/ExampleUnitTest.kt
+++ Frontend/Android/app/src/test/java/com/team03/cocktailrecipesapp/ExampleUnitTest.kt
@@ -5,53 +5,115 @@ import org.junit.Assert
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.time.LocalDateTime
 
-//TODO import
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
+ * Local unit test
+ * checking for correct parsing of the json strings recieved from server
  */
 class ExampleUnitTest {
+
+    private val server = serverAPI();
+
+    data class AnswerSuccess(
+            var success: Int? = -1
+    )
+
+    data class AnswerUserID(
+            var user_id: Int? = -1
+    )
+
+    data class Recipe(
+            var name: String? = null,
+            var preptime_minutes: Int,
+            var difficulty: Int,
+            var instruction: String? = null,
+            var creation_time: LocalDateTime? = null,
+            var creator_username: String? = null
+    )
+
+    data class AnswerRecipes(
+            var recipes: List<Recipe>
+    )
+
     @Test
     fun register_checkAPI_call() {
-        val call = serverAPI.register("username", "password_hash");
-        Assert.assertEquals("{ success: 0 }", call);
+        val string = server.register("username", "password_hash");
+        val answer = Gson().fromJson(string, AnswerSuccess::class.java);
+
+        Assert.assertNotNull(answer)
+        Assert.assertEquals(0, answer.success)
     }
 
     fun login_checkAPI_call() {
-        val call = serverAPI.login("username", "password_hash");
-        Assert.assertEquals(call, "{ user_id: 0 }");
-    }
+        val string = server.login("username", "password_hash");
+        val answer = Gson().fromJson(string, AnswerUserID::class.java);
 
-    fun addRecipe_checkAPI_call() {
-        val call = serverAPI.addRecipe("Freddy Krueger", 5, 1, "Fill in all spirits and mix up.", listOf("STROH 80","Sambuca","Jägermeister"));
-        Assert.assertEquals(call, "{ success: 0 }");
-    }
-
-    fun rateRecipe_checkAPI_call() {
-        val call = serverAPI.rateRecipe(1, 1, 1);
-        Assert.assertEquals(call, "{ success: 0 }");
+        Assert.assertNotNull(answer)
+        Assert.assertEquals(0, answer.user_id)
     }
 
     fun getRecipes_checkAPI_call() {
-        val call = serverAPI.getRecipes();
-        Assert.assertEquals(call, "{ success: 0 }");
+        val string = server.getRecipes();
+        val answer = Gson().fromJson(string, AnswerRecipes::class.java);
+
+        Assert.assertNotNull(answer)
+        Assert.assertTrue(answer.recipes.isNotEmpty())
+
+        for (recipe in answer.recipes)
+        {
+            Assert.assertNotNull(recipe.creation_time);
+            Assert.assertNotNull(recipe.creator_username);
+            Assert.assertNotNull(recipe.instruction);
+            Assert.assertNotNull(recipe.name);
+
+            Assert.assertTrue(recipe.difficulty > 0)
+            Assert.assertTrue(recipe.difficulty <= 10)
+        }
     }
 
     fun changePassword_checkAPI_call() {
-        val call = serverAPI.changePassword("password_hash_old", "password_hash_new");
-        Assert.assertEquals(call, "{ success: 0 }");
+        val string = server.changePassword("old_password_has", "new_password_hash");
+        val answer = Gson().fromJson(string, AnswerSuccess::class.java);
+
+        Assert.assertNotNull(answer)
+        Assert.assertEquals(0, answer.success)
     }
 
     fun deleteRecipe_checkAPI_call() {
-        val call = serverAPI.deleteRecipe(1, 1);
-        Assert.assertEquals(call, "{ success: 0 }");
+        val string = server.deleteRecipe(-1, -1);
+        val answer = Gson().fromJson(string, AnswerSuccess::class.java);
+
+        Assert.assertNotNull(answer)
+        Assert.assertEquals(0, answer.success)
     }
 
     fun likeRecipe_checkAPI_call() {
-        val call = serverAPI.likeRecipe(1, 1);
-        Assert.assertEquals(call, "{ success: 0 }");
+        val string = server.likeRecipe(-1, -1);
+        val answer = Gson().fromJson(string, AnswerSuccess::class.java);
+
+        Assert.assertNotNull(answer)
+        Assert.assertEquals(0, answer.success)
     }
+
+    fun addRecipe_checkAPI_call() {
+        val string = server.addRecipe(1, "Freddy Krueger", 5,
+                "First put in Stroh80 in 1 2cl shot glass. Take a teaspoon and " +
+                        "layer the Jaeger onto the rum. Then finally do the same with the " +
+                        "Baileys. Enjoy!", listOf("Stroh80", "Jaegermeister", "Baileys"));
+        val answer = Gson().fromJson(string, AnswerSuccess::class.java);
+
+        Assert.assertNotNull(answer)
+        Assert.assertTrue(answer.success == 0 || answer.success == 1)
+    }
+
+    fun rateRecipe_checkAPI_call() {
+        val string = server.likeRecipe(-1, -1);
+        val answer = Gson().fromJson(string, AnswerSuccess::class.java);
+
+        Assert.assertNotNull(answer)
+        Assert.assertEquals(0, answer.success)
+    }
+
 }
