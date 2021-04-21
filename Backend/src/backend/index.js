ceb6d5f54d2ceb363f4e17e448d45a34f744d2c6
xsoliah <hess@student.tugraz.at>
2021-04-21 11:46:18 +0200

    refactored rest

--- Backend/src/backend/index.js
+++ Backend/src/backend/index.js
@@ -25,116 +25,75 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send("Hello from the API!"); 
-});
-
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
   console.log("register called!");
   let name = req.body.name;
   let password = req.body.password_hash;
-  con.beginTransaction(function(err) {
-    if (err) { throw err; }
-    // first check if user with given name already exists
-    con.query('SELECT * FROM User WHERE name=\''+name+'\'', function(err, result) {
-      if (err) { 
-        con.rollback(function() {
-          throw err;
-        });
-      }
-      if (result.length > 0) {
-        // user already exists
-        console.log("User with name="+name+" already exists!");
-        res.send({
-          success: false
-        });
-        return;
-      }
-      else {
-        // create new user
-        con.query('INSERT into User (name, password, is_admin) values (\''+name+'\',\''+password+'\', 0)', function (err, result) {
-          // check if mysql throws an error
-          if (err) {
-            res.send({
-              success: false
-            });
-            return;
-          }
-
-          // success!
-          res.send({
-            success: true
-          });
-
-          return;
-        });
-      }
+  // first check if user with given name already exists
+  var result = await query('SELECT * FROM User WHERE name=\''+name+'\'');
+  if (result.length > 0) {
+    // user already exists
+    console.log("User with name="+name+" already exists!");
+    res.send({
+      success: false
     });
-  });
+    return;
+  }
+  else {
+    // create new user
+    await query('INSERT into User (name, password, is_admin) values (\''+name+'\',\''+password+'\', 0)');
+    
+    // success!
+    res.send({
+      success: true
+    });
+    return;
+  }
 });
 
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
   console.log("login called!");
   let name = req.body.name;
   let password = req.body.password_hash;
-  con.beginTransaction(function(err) {
-    if (err) { throw err; }
     // first check if user with given name already exists
-    var query = 'SELECT * FROM User WHERE name=\''+name+'\' AND password=\''+password+'\'';
-    console.log(query);
-    con.query(query, function(err, result) {
-      if (err) { 
-        con.rollback(function() {
-          throw err;
-        });
-      }
-      if (result.length > 0) {
-        // user found
-        console.log("login for user with id="+result[0].id+" worked fine!");
-        res.send({
-          user_id: result[0].id
-        });
-        return;
-      }
-      else {
-        // user not found or password was wrong
-        console.log("login failed -> wrong username or password");
-        res.send({
-          user_id: -1
-        });
-        return;
-      }
-    });
-  });
+    var result = await query('SELECT * FROM User WHERE name=\''+name+'\' AND password=\''+password+'\''); 
+    if (result.length > 0) {
+      // user found
+      console.log("login for user with id="+result[0].id+" worked fine!");
+      res.send({
+        user_id: result[0].id
+      });
+      return;
+    }
+    else {
+      // user not found or password was wrong
+      console.log("login failed -> wrong username or password");
+      res.send({
+        user_id: -1
+      });
+      return;
+    }
 });
 
-app.get('/get-recipes', (req, res) => {
+app.get('/get-recipes', async (req, res) => {
   console.log("get-recipes called!");
-  con.beginTransaction(function(err) {
-    if (err) { throw err; }
-    // select all recipes
-    con.query('SELECT r.name as recipe_name, r.preptime_minutes, r.difficulty, r.instruction, r.created_at, u.name as user_name FROM Recipe r JOIN User u on r.creator_id = u.id', function(err, result) {
-      if (err) { 
-        con.rollback(function() {
-          throw err;
-        });
-      }
-      console.log(result);
-      var out = [];
-      for (var i in result) {
-        out.push({
-          name: result[i].recipe_name,
-          preptime_minutes: result[i].preptime_minutes,
-          difficulty: result[i].difficulty,
-          instruction: result[i].instruction,
-          creation_time: result[i].created_at,
-          creator_user: result[i].user_name
-        });
-      }
-
-      res.send(out);
+  // select all recipes
+  var result = await query('SELECT r.name as recipe_name, r.preptime_minutes, r.difficulty, r.instruction, r.created_at, u.name as user_name FROM Recipe r JOIN User u on r.creator_id = u.id');
+    
+  console.log(result);
+  var out = [];
+  for (var i in result) {
+    out.push({
+      name: result[i].recipe_name,
+      preptime_minutes: result[i].preptime_minutes,
+      difficulty: result[i].difficulty,
+      instruction: result[i].instruction,
+      creation_time: result[i].created_at,
+      creator_user: result[i].user_name
     });
-  });
+  }
+
+  res.send(out);
 });
 
 app.post('/add-recipe', async (req, res) => {
