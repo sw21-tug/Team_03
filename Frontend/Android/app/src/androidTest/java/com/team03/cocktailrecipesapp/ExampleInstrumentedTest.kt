4997b0dd923a477b50c86dec99b37570d790c1c8
bernhard schnoegl <bernhard.schnoegl@student.tugraz.at>
2021-04-21 12:01:38 +0200

    CRA-FE-5 userregistration and validation tests [KL,SB]

--- Frontend/Android/app/src/androidTest/java/com/team03/cocktailrecipesapp/ExampleInstrumentedTest.kt
+++ Frontend/Android/app/src/androidTest/java/com/team03/cocktailrecipesapp/ExampleInstrumentedTest.kt
@@ -1,19 +1,16 @@
 package com.team03.cocktailrecipesapp
 
+import android.widget.Button
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.junit.Assert
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Rule
-
-
-import com.team03.cocktailrecipesapp.ui.login.LoginActivity
-import org.junit.Assert
+import java.util.regex.Pattern.matches
 
 
 //"../../com.team03.cocktailrecipesapp/ui.login/LoginViewModel"
@@ -26,27 +23,25 @@ import org.junit.Assert
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
 
-    private fun validateInput(): Boolean {
-        return false;
-    }
-
     @get:Rule
-    val activityRule = ActivityScenarioRule(LoginActivity::class.java)
-
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
     fun loginDataTest(){
+        onView(withId(R.id.btnSwitchToLogin)).perform(click())
         onView(withId(R.id.etUsername)).perform(typeText("Testusername"), closeSoftKeyboard())
         onView(withId(R.id.etPassword)).perform(typeText("Testpassword"), closeSoftKeyboard())
     }
 
     @Test
     fun loginButton() {
+        onView(withId(R.id.btnSwitchToLogin)).perform(click())
         onView(withId(R.id.btnLogin)).perform(click())
     }
 
     @Test
     fun registerButton() {
+        onView(withId(R.id.btnSwitchToLogin)).perform(click())
         onView(withId(R.id.btnRegister)).perform(click())
     }
 
@@ -57,34 +52,20 @@ class ExampleInstrumentedTest {
         onView(withId(R.id.txt_username)).perform(typeText("John Doe"), closeSoftKeyboard())
         onView(withId(R.id.txt_password)).perform(typeText("password1"), closeSoftKeyboard())
         onView(withId(R.id.txt_password_repeat)).perform(typeText("password1"), closeSoftKeyboard())
-        Assert.assertEquals(validateInput(), true);
     }
 
     @Test
     fun fillOutRegistrationform_ErrorPasswordRepeat() {
-        onView(withId(R.id.btnRegister)).perform(click())
-        onView(withId(R.id.txt_username)).perform(typeText("John Doe"), closeSoftKeyboard())
-        onView(withId(R.id.txt_password)).perform(typeText("password1"), closeSoftKeyboard())
-        onView(withId(R.id.txt_password_repeat)).perform(typeText("differentPassword"), closeSoftKeyboard())
-        Assert.assertEquals(validateInput(), false);
-
+        Assert.assertEquals(RegisterFuncs().validateInput("John Doe", "password1", "password2"), -4);
     }
 
     @Test
     fun fillOutRegistrationform_ErrorEmptyPassword() {
-        onView(withId(R.id.btnRegister)).perform(click())
-        onView(withId(R.id.txt_username)).perform(typeText("John Doe"), closeSoftKeyboard())
-        onView(withId(R.id.txt_password)).perform(typeText(""), closeSoftKeyboard())
-        onView(withId(R.id.txt_password_repeat)).perform(typeText("password1"), closeSoftKeyboard())
-        Assert.assertEquals(validateInput(), false);
+        Assert.assertEquals(RegisterFuncs().validateInput("John Doe", "", "password2"), -2);
     }
 
     @Test
     fun fillOutRegistrationform_ErrorEmptyPasswordRepeat() {
-        onView(withId(R.id.btnRegister)).perform(click())
-        onView(withId(R.id.txt_username)).perform(typeText("John Doe"), closeSoftKeyboard())
-        onView(withId(R.id.txt_password)).perform(typeText("password1"), closeSoftKeyboard())
-        onView(withId(R.id.txt_password_repeat)).perform(typeText(""), closeSoftKeyboard())
-        Assert.assertEquals(validateInput(), false);
+        Assert.assertEquals(RegisterFuncs().validateInput("John Doe", "", "password2"), -3);
     }
 }
