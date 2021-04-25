14d250a22413d0cf815716cbe7d5b7cd6d0a8649
Christoph Staubmann <christoph.staubmann@student.tugraz.at>
2021-04-25 19:15:03 +0200

    CRA-FE-3 cocktail list layout finished [SC,SD]

--- Frontend/Android/app/src/main/java/com/team03/cocktailrecipesapp/MainActivity.kt
+++ Frontend/Android/app/src/main/java/com/team03/cocktailrecipesapp/MainActivity.kt
@@ -3,36 +3,35 @@ package com.team03.cocktailrecipesapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
-import kotlinx.android.synthetic.main.cocktail_list_view.*
-import kotlinx.android.synthetic.main.cocktail_list_view_card.view.*
+import kotlinx.android.synthetic.main.trending_cocktail_list.*
+import kotlinx.android.synthetic.main.trending_cocktail_list_card.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        addRecipesToList(getRecipes())
+
+
+        val recipe_list = getRecipes()
+        fillTrendingRecipesList(recipe_list)
     }
 
-    fun addRecipesToList(recipe_list: List<Recipe>) : Boolean {
+    fun fillTrendingRecipesList(recipe_list: List<Recipe>) {
         recipe_list.forEach() {
-            val recipeCard = LayoutInflater.from(this).inflate(R.layout.cocktail_list_view_card, cocktail_list, false)
+            val recipeCard = LayoutInflater.from(this).inflate(R.layout.trending_cocktail_list_card, trending_cocktail_list, false)
             recipeCard.cocktail_name.text = it.name
-            recipeCard.cocktail_ratings.text = "0"
-            recipeCard.cocktail_rating_bar.rating = 3F
+            recipeCard.cocktail_ratings.text = it.times_rated.toString()
+            recipeCard.cocktail_rating_bar.rating = it.rating
             recipeCard.cocktail_difficulty.text =  it.difficulty.toString()
             recipeCard.cocktail_preparation_time.text = it.preptime_minutes.toString()
             /*recipeCard.cocktail_image*/
-            cocktail_list.addView(recipeCard)
+            trending_cocktail_list.addView(recipeCard)
         }
-        return true
     }
 
     fun getRecipes() : List<Recipe> {
-        val recipe_1 = Recipe("cocktail 1")
-        val recipe_2 = Recipe("cocktail 2")
-        val recipe_3 = Recipe("cocktail 3")
-        val recipe_4 = Recipe("cocktail 4")
-        val recipe_list = listOf(recipe_1, recipe_2, recipe_3,recipe_4)
+        val recipe = Recipe("cocktail XYZ", 4.0F, 2)
+        val recipe_list = listOf(recipe, recipe, recipe, recipe, recipe, recipe)
         return recipe_list
     }
 }
--- Frontend/Android/app/src/test/java/com/team03/cocktailrecipesapp/RecipeListTest.kt
+++ Frontend/Android/app/src/test/java/com/team03/cocktailrecipesapp/RecipeListTest.kt
@@ -7,9 +7,9 @@ class RecipeListTest
 {
     @Test
     fun getRecipes() {
-        val recipe_1 = Recipe("cocktail 1")
-        val recipe_2 = Recipe("cocktail 2")
-        val recipe_3 = Recipe("cocktail 3")
+        val recipe_2 = Recipe("cocktail 2", 4.0F, 1)
+        val recipe_3 = Recipe("cocktail 3", 4.0F, 1)
+        val recipe_1 = Recipe("cocktail 1", 4.0F, 1)
         val recipe_list = listOf(recipe_1, recipe_2, recipe_3)
 
         recipe_list.forEach {
@@ -19,6 +19,8 @@ class RecipeListTest
             assertNotNull("recipe: instruction <str> invalid!", it.instruction.length)
             assertNotNull("recipe: creation_time <str> invalid!", it.creation_time.length)
             assertNotNull("recipe: creator_username <str> invalid!", it.creator_username.length)
+            assertNotNull("recipe: rating <float> invalid!", 0.0F < it.rating && it.rating < 5.0F)
+            assertNotNull("recipe: times_rated <int> invalid!", it.times_rated >= 0)
         }
     }
 }
