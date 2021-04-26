12029d52bf28b6d935df8e0b9d9aab31e3c867ba
michael_schaffer <michael.schaffe@student.tugraz.at>
2021-04-26 17:15:54 +0200

    FE-4 Add listeners [SM,SM]

--- Frontend/Android/app/src/main/java/com/team03/cocktailrecipesapp/serverAPI.kt
+++ Frontend/Android/app/src/main/java/com/team03/cocktailrecipesapp/serverAPI.kt
@@ -10,11 +10,17 @@ import com.android.volley.toolbox.JsonRequest
 import com.android.volley.toolbox.Volley
 import org.json.JSONObject
 
+
+/*
+* HowTo: Create object of class. To communicate with the server just call one of the methods. Pass the informations.
+* Important part is, that you have to create the Listeners before calling the methods so you can handle the response yourself.
+* Request will be send by the methods.
+ */
 class serverAPI(context: Context)
 {
     val context_ = context
 
-    fun register(username: String, password_hash: String) : String
+    fun register(username: String, password_hash: String, listener: Response.Listener<JSONObject>, error_listener: Response.ErrorListener) : Int
     {
         val register_json = """
             {
@@ -24,10 +30,10 @@ class serverAPI(context: Context)
             """
 
         val json = JSONObject(register_json)
-        return sendRequest(json).toString()
+        return sendRequest(json, listener, error_listener)
     }
 
-    fun login(username: String, password_hash: String) : String
+    fun login(username: String, password_hash: String, listener: Response.Listener<JSONObject>, error_listener: Response.ErrorListener) : Int
     {
         val login_json = """
             {
@@ -37,10 +43,10 @@ class serverAPI(context: Context)
             """
 
         val json = JSONObject(login_json)
-        return sendRequest(json).toString()
+        return sendRequest(json, listener, error_listener)
     }
 
-    fun addRecipe(user_id: Int, name: String, preptime_minutes: Int, difficulty: Int, instruction: String, ingredient_names: List<String>) : String
+    fun addRecipe(user_id: Int, name: String, preptime_minutes: Int, difficulty: Int, instruction: String, ingredient_names: List<String>, listener: Response.Listener<JSONObject>, error_listener: Response.ErrorListener) : Int
     {
         var add_recipe_json = """
             {
@@ -65,10 +71,10 @@ class serverAPI(context: Context)
         }
 
         val json = JSONObject(add_recipe_json)
-        return sendRequest(json).toString()
+        return sendRequest(json, listener, error_listener)
     }
 
-    fun rateRecipe(user_id: Int, recipe_id: Int, value: Int) : String
+    fun rateRecipe(user_id: Int, recipe_id: Int, value: Int, listener: Response.Listener<JSONObject>, error_listener: Response.ErrorListener) : Int
     {
         val rate_recipe_json = """
             {
@@ -79,16 +85,16 @@ class serverAPI(context: Context)
             """
 
         val json = JSONObject(rate_recipe_json)
-        return sendRequest(json).toString()
+        return sendRequest(json, listener, error_listener)
     }
 
-    fun getRecipes() : String
+    fun getRecipes(listener: Response.Listener<JSONObject>, error_listener: Response.ErrorListener) : Int
     {
         val json = JSONObject()
-        return sendRequest(json).toString()
+        return sendRequest(json, listener, error_listener)
     }
 
-    fun changePassword(user_id: Int, old_password_hash: String, new_password_hash: String) : String
+    fun changePassword(user_id: Int, old_password_hash: String, new_password_hash: String, listener: Response.Listener<JSONObject>, error_listener: Response.ErrorListener) : Int
     {
         val change_password_json = """
             {
@@ -99,10 +105,10 @@ class serverAPI(context: Context)
             """
 
         val json = JSONObject(change_password_json)
-        return sendRequest(json).toString()
+        return sendRequest(json, listener, error_listener)
     }
 
-    fun deleteRecipe(user_id: Int, recipe_id: Int) : String
+    fun deleteRecipe(user_id: Int, recipe_id: Int, listener: Response.Listener<JSONObject>, error_listener: Response.ErrorListener) : Int
     {
         val delete_recipe_json = """
             {
@@ -112,10 +118,10 @@ class serverAPI(context: Context)
             """
 
         val json = JSONObject(delete_recipe_json)
-        return sendRequest(json).toString()
+        return sendRequest(json, listener, error_listener)
     }
 
-    fun likeRecipe(user_id: Int, recipe_id: Int) : String
+    fun likeRecipe(user_id: Int, recipe_id: Int, listener: Response.Listener<JSONObject>, error_listener: Response.ErrorListener) : Int
     {
         val like_recipe_json = """
             {
@@ -125,25 +131,19 @@ class serverAPI(context: Context)
             """
 
         val json = JSONObject(like_recipe_json)
-        return sendRequest(json).toString()
+        return sendRequest(json, listener, error_listener)
     }
 
-    private fun sendRequest(jsonObject: JSONObject) : JSONObject
+    private fun sendRequest(jsonObject: JSONObject, listener: Response.Listener<JSONObject>, error_listener: Response.ErrorListener) : Int
     {
         val queue = Volley.newRequestQueue(context_)
         val url = "localhost:3000"
 
-        var response = JSONObject()
-
-        val request = JsonObjectRequest(url, jsonObject, {
-            response = it
-        }, {
-            print("Error: No connection possible\n")
-        })
+        val request = JsonObjectRequest(url, jsonObject, listener, error_listener)
 
         queue.add(request)
         queue.start()
 
-        return response
+        return 0
     }
 }
