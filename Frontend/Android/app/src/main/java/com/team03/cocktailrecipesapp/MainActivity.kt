d1fad621a40faeb70a87fe573bf425c06feeea95
Christoph Staubmann <christoph.staubmann@student.tugraz.at>
2021-05-04 19:57:09 +0200

    CRA-FE-7 listeners for getRecipes server call [SC,SB]

--- Frontend/Android/app/src/main/java/com/team03/cocktailrecipesapp/MainActivity.kt
+++ Frontend/Android/app/src/main/java/com/team03/cocktailrecipesapp/MainActivity.kt
@@ -4,14 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-
+import com.team03.cocktailrecipesapp.recipes.GetRecipesErrorListener
+import com.team03.cocktailrecipesapp.recipes.GetRecipesListener
 import com.team03.cocktailrecipesapp.ui.login.LoginActivity
-import android.view.LayoutInflater
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.recommended_cocktail_list.*
-import kotlinx.android.synthetic.main.trending_cocktail_list.*
-import kotlinx.android.synthetic.main.trending_cocktail_list_card.view.*
+import com.team03.cocktailrecipesapp.recipes.Recipe
 
 //TODO: -> move to shared preferences
 var userLoggedIn = false;
@@ -29,44 +25,43 @@ class MainActivity : AppCompatActivity() {
         else {
             setContentView(R.layout.activity_main)
 
-
-            val recipe_list = getRecipes()
-            val recommended_recipes = getRecommendedRecipes(recipe_list)
-
-            fillTrendingRecipesList(recipe_list)
-            fillRecommendedRecipesList(recommended_recipes)
+            // Get recipes from server and inflate list
+            fillTrendingRecipesList()
         }
     }
 
-    fun fillTrendingRecipesList(recipe_list: List<Recipe>) {
-        recipe_list.forEach() {
-            val recipeCard = LayoutInflater.from(this).inflate(R.layout.trending_cocktail_list_card, trending_cocktail_list, false)
-            recipeCard.cocktail_name.text = it.name
-            recipeCard.cocktail_ratings.text = it.times_rated.toString()
-            recipeCard.cocktail_rating_bar.rating = it.rating
-            recipeCard.cocktail_difficulty.text =  it.difficulty.toString()
-            val preparation_time:String = it.preptime_minutes.toString() + " minutes"
-            recipeCard.cocktail_preparation_time.text = preparation_time
-            /*recipeCard.cocktail_image*/
-            trending_cocktail_list.addView(recipeCard)
-        }
+    fun fillTrendingRecipesList() {
+        val server = serverAPI(applicationContext)
+        val listener = GetRecipesListener(::onSuccessfulGetRecipes)
+        val errorListener = GetRecipesErrorListener(::onFailedGetRecipes)
+        server.getRecipes(listener, errorListener)
     }
 
-    fun fillRecommendedRecipesList(recommended_recipes: List<Recipe>) : Boolean {
-        // TODO: inflate recommeneded recipes
-        return false
+    fun onSuccessfulGetRecipes(recipe_list: List<Recipe>) {
+        println("Get recipes() success...")
+        // TODO: inflate list
+        recipe_list.forEach { recipe -> println("$recipe") }
     }
 
-    fun getRecipes() : List<Recipe> {
-        val recipe = Recipe("cocktail XYZ", 4.0F, 2)
-        val recipe_list = listOf(recipe, recipe, recipe, recipe, recipe, recipe)
-        return recipe_list
+    fun onFailedGetRecipes() {
+        // TODO: display error message
+        println("Get recipes() failed...")
     }
 
-    fun getRecommendedRecipes(recipe_list: List<Recipe>) : List<Recipe> {
-        // TODO: get recommended recipes (top 5 rated)
-        return recipe_list
-    }
+//    fun fillTrendingRecipesList(recipe_list: List<Recipe>) {
+//        recipe_list.forEach() {
+//            val recipeCard = LayoutInflater.from(this).inflate(R.layout.trending_cocktail_list_card, trending_cocktail_list, false)
+//            recipeCard.cocktail_name.text = it.name
+//            recipeCard.cocktail_ratings.text = it.times_rated.toString()
+//            recipeCard.cocktail_rating_bar.rating = it.rating
+//            recipeCard.cocktail_difficulty.text =  it.difficulty.toString()
+//            val preparation_time:String = it.preptime_minutes.toString() + " minutes"
+//            recipeCard.cocktail_preparation_time.text = preparation_time
+//            /*recipeCard.cocktail_image*/
+//            trending_cocktail_list.addView(recipeCard)
+//        }
+//    }
+
 
     fun loginOnClick(view: View){
             val intent = Intent(this, LoginActivity::class.java)
