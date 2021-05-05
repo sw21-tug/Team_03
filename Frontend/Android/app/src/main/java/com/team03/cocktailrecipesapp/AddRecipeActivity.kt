9b00542cc9acd5861421d68acca32e091010483d
Lorenz Kutschka <lorenz.kutschka@student.tugraz.at>
2021-05-05 10:44:43 +0200

    CRA-FE-15 add recipe activity [KL,SC]

---
+++ Frontend/Android/app/src/main/java/com/team03/cocktailrecipesapp/AddRecipeActivity.kt
@@ -0,0 +1,11 @@
+package com.team03.cocktailrecipesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddRecipeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_recipe)
+    }
+}
