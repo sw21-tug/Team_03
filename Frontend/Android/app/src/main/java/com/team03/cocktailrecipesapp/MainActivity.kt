c2519d5905f0481864f064fdbb2de6183b6f88d1
Christoph Staubmann <christoph.staubmann@student.tugraz.at>
2021-05-05 08:36:27 +0200

    CRA-FE-9 recipes list add error msg [SC,SB]

--- Frontend/Android/app/src/main/java/com/team03/cocktailrecipesapp/MainActivity.kt
+++ Frontend/Android/app/src/main/java/com/team03/cocktailrecipesapp/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.Toast
 import com.team03.cocktailrecipesapp.recipes.GetRecipesErrorListener
 import com.team03.cocktailrecipesapp.recipes.GetRecipesListener
 import com.team03.cocktailrecipesapp.ui.login.LoginActivity
 import com.team03.cocktailrecipesapp.recipes.Recipe
 import kotlinx.android.synthetic.main.progress_indicator.*
+import kotlinx.android.synthetic.main.error_msg_indicator.*
 import kotlinx.android.synthetic.main.trending_cocktail_list.*
 import kotlinx.android.synthetic.main.trending_cocktail_list_card.view.*
 
@@ -42,27 +44,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onSuccessfulGetRecipes(recipe_list: List<Recipe>) {
-        println("Get recipes() success...")
-        // TODO: inflate list
+        progressBar.visibility = View.GONE
         fillTrendingRecipesList(recipe_list)
     }
 
     fun onFailedGetRecipes() {
-        // TODO: display error message
-        println("Get recipes() failed...")
+        txtViewErrorMsg.text = resources.getString(R.string.failed_to_load_recipes_from_server)
+        txtViewErrorMsgContainer.visibility = View.VISIBLE
+        Toast.makeText(applicationContext, resources.getString(R.string.failed_to_load_recipes_from_server), Toast.LENGTH_LONG).show()
     }
 
     fun fillTrendingRecipesList(recipe_list: List<Recipe>) {
-        progressBar.visibility = View.GONE
-        recipe_list.forEach() {
+        recipe_list.forEach() { recipe ->
             val recipeCard = LayoutInflater.from(this).inflate(R.layout.trending_cocktail_list_card, trending_cocktail_list, false)
-            recipeCard.cocktail_name.text = it.name
-            recipeCard.cocktail_ratings.text = it.times_rated.toString()
-            recipeCard.cocktail_rating_bar.rating = it.rating
-            recipeCard.cocktail_difficulty.text =  it.difficulty.toString()
-            val preparation_time:String = it.preptime_minutes.toString() + " minutes"
-            recipeCard.cocktail_preparation_time.text = preparation_time
-            /*recipeCard.cocktail_image*/
+            recipeCard.cocktail_name.text = recipe.name
+            recipeCard.cocktail_ratings.text = recipe.times_rated.toString()
+            recipeCard.cocktail_rating_bar.rating = recipe.rating
+            recipeCard.cocktail_difficulty.text =  recipe.difficulty.toString()
+            val preparationTime: String = recipe.preptime_minutes.toString() + " minutes"
+            recipeCard.cocktail_preparation_time.text = preparationTime
+            /* TODO: recipeCard.cocktail_image */
             trending_cocktail_list.addView(recipeCard)
         }
     }
