a3d60d01c3fe6e66103ab256e536d86cb58bd8f9
michael_schaffer <michael.schaffe@student.tugraz.at>
2021-05-05 09:36:10 +0200

    CRA-FE-14 Create cocktail details activity [SM,SB]

---
+++ Frontend/Android/app/src/main/java/com/team03/cocktailrecipesapp/CocktailDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.team03.cocktailrecipesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CocktailDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cocktail_detail)
+    }
+}
