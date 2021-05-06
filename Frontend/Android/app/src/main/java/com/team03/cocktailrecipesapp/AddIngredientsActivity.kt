556274a2fa058beae87bb5033a418f5e2a638149
Lorenz Kutschka <lorenz.kutschka@student.tugraz.at>
2021-05-06 13:08:51 +0200

    CRA-FE-15 add recipe without ingredients [KL,SC]

--- Frontend/Android/app/src/main/java/com/team03/cocktailrecipesapp/AddIngredientsActivity.kt
+++ Frontend/Android/app/src/main/java/com/team03/cocktailrecipesapp/AddIngredientsActivity.kt
@@ -1,13 +1,16 @@
 package com.team03.cocktailrecipesapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.forEach
 import com.team03.cocktailrecipesapp.recipes.GetIngredientsErrorListener
 import com.team03.cocktailrecipesapp.recipes.GetIngredientsListener
 import com.team03.cocktailrecipesapp.recipes.Ingredient
+import com.team03.cocktailrecipesapp.ui.login.LoginActivity
 import kotlinx.android.synthetic.main.error_msg_indicator.*
 import kotlinx.android.synthetic.main.ingredients_item.view.*
 import kotlinx.android.synthetic.main.ingredients_list.*
@@ -37,16 +40,7 @@ class AddIngredientsActivity : AppCompatActivity() {
             ingredientView.ingredientItem.text = ingredient.name
             ingredientsList.addView(ingredientView)
         }
-        ingredients.forEach { ingredient ->
-            val ingredientView = LayoutInflater.from(this).inflate(R.layout.ingredients_item, ingredientsList, false)
-            ingredientView.ingredientItem.text = ingredient.name
-            ingredientsList.addView(ingredientView)
-        }
-        ingredients.forEach { ingredient ->
-            val ingredientView = LayoutInflater.from(this).inflate(R.layout.ingredients_item, ingredientsList, false)
-            ingredientView.ingredientItem.text = ingredient.name
-            ingredientsList.addView(ingredientView)
-        }
+
     }
 
     fun onFailedGetIngredient() {
@@ -55,5 +49,32 @@ class AddIngredientsActivity : AppCompatActivity() {
         Toast.makeText(applicationContext, resources.getString(R.string.failed_to_load_ingredients_from_server), Toast.LENGTH_LONG).show()
 
         // TODO: get ingredients from shared preferences
+    }
+
+    fun onClickAddIngredients(view: View) {
+/*
+        val picked_ingredients: Array<String> = arrayOf()
+        val ingredientView = LayoutInflater.from(this).inflate(R.layout.ingredients_item, ingredientsList, false)
+        var index: Int = 0
+        ingredientsList.forEach { ingredient ->
+            if(ingredient.ingredientItem.isChecked)
+            {
+                picked_ingredients[index++] = ingredient.ingredientItem.text.toString()
+                //picked_ingredients.add(ingredient.ingredientItem.text.toString())
+            }
+        }
+
+        picked_ingredients.forEach{
+            println(it)
+        }
+
+        val intent = Intent()
+        intent.putExtra("Pickedingredients", picked_ingredients)
+        intent.putExtra("Pickedingredients", picked_ingredients)
+        setResult(RESULT_OK, intent)
+        finish()
+        */
+        val intent = Intent(this, AddRecipeActivity::class.java)
+        startActivity(intent)
     }
 }
--- Frontend/Android/app/src/main/java/com/team03/cocktailrecipesapp/AddRecipeActivity.kt
+++ Frontend/Android/app/src/main/java/com/team03/cocktailrecipesapp/AddRecipeActivity.kt
@@ -1,13 +1,19 @@
 package com.team03.cocktailrecipesapp
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.team03.cocktailrecipesapp.ui.login.LoginActivity
 import kotlinx.android.synthetic.main.activity_add_recipe.*
 
 
 class AddRecipeActivity : AppCompatActivity() {
+
+    var ingredients_pub: List<String>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +25,50 @@ class AddRecipeActivity : AppCompatActivity() {
         difficulty_picker.maxValue = 5
     }
 
+    fun onSuccessfullAddRecipe() {
+
+        //this.finish()
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onUnsuccessfullAddRecipe() {
+        Toast.makeText(applicationContext, resources.getString(R.string.failed_to_add_recipe), Toast.LENGTH_LONG).show()
+    }
+
     fun onClickManageIngredients(view: View) {
+
         val intent = Intent(this, AddIngredientsActivity::class.java)
         startActivity(intent)
+    }
+
+    /*
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (data != null)
+        {
+            ingredients_pub = data.getStringArrayExtra("Pickedingredients")?.toList()
+        }
+        ingredients_pub?.forEach { println(it) }
+    }
+    */
+
+    fun onClickAddRecipe(view: View) {
+        val server = serverAPI(applicationContext)
+        val listener = AddRecipeListener(::onSuccessfullAddRecipe)
+        val errorListener = AddRecipeErrorListener(::onUnsuccessfullAddRecipe)
+
+        val shared: SharedPreferences = getSharedPreferences("Settings", Context.MODE_PRIVATE)
+
+        ingredients_pub = listOf("Rum", "Vodka")
+
+        val recipe_name: String = etRecipeName.text.toString()
+        val recipe_description: String = etRecipeDescription.text.toString()
+        val preperation_time: Int = timer_picker_minutes.value
+        val difficulty: Int = difficulty_picker.value
+
+
+        server.addRecipe(shared.getInt("userId", 0), recipe_name, preperation_time, difficulty, recipe_description, ingredients_pub.orEmpty(), listener, errorListener)
     }
 }
