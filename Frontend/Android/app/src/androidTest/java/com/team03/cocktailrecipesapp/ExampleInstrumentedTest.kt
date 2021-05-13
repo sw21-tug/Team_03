fb160c6eb6f913244fc2fa08960afb9ab1c03e01
michael_schaffer <michael.schaffer@student.tugraz.at>
2021-05-13 15:15:19 +0200

    DEV-FE-18 Add delete recipe button tests [SM,HM]

--- Frontend/Android/app/src/androidTest/java/com/team03/cocktailrecipesapp/ExampleInstrumentedTest.kt
+++ Frontend/Android/app/src/androidTest/java/com/team03/cocktailrecipesapp/ExampleInstrumentedTest.kt
@@ -9,6 +9,7 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.android.volley.Response
+import org.hamcrest.core.IsNot.not
 import org.json.JSONObject
 import org.junit.Assert
 import org.junit.Rule
@@ -188,17 +189,11 @@ class ExampleInstrumentedTest {
 
         onView(withId(R.id.etUsername)).perform(typeText("UsernameTest123"), closeSoftKeyboard())
         onView(withId(R.id.etPassword)).perform(typeText("password123"), closeSoftKeyboard())
-        onView(withId(R.id.btnBack)).perform(click())
+        onView(withId(R.id.btnLogin)).perform(click())
     }
 
-    @Test
+
     fun PerformLogout() {
-        // login
-
-        //onView(withId(R.id.etUsername)).perform(typeText("daniel"), closeSoftKeyboard())
-        //onView(withId(R.id.etPassword)).perform(typeText("1234qwer"), closeSoftKeyboard())
-        //onView(withId(R.id.btnLogin)).perform(click())
-
         // user-profile
         onView(withId(R.id.imgBtnAvatar)).perform(click())
 
@@ -207,6 +202,57 @@ class ExampleInstrumentedTest {
         onView(withId(R.id.logoutButton)).perform(click())
 
         // check if we are in login page
-        onView(withId(R.id.btnBack)).check(matches(isDisplayed()))
+        onView(withId(R.id.btnLogin)).check(matches(isDisplayed()))
+    }
+
+    fun PerformLogin(username: String, password: String)
+    {
+        if (userId > 0)
+            PerformLogout()
+
+        onView(withId(R.id.etUsername)).perform(typeText(username), closeSoftKeyboard())
+        onView(withId(R.id.etPassword)).perform(typeText(password), closeSoftKeyboard())
+        onView(withId(R.id.btnLogin)).perform(click())
+
+    }
+
+    fun PerformLoginAsNonAdmin()
+    {
+        PerformLogin("osama", "1234qwer")
+    }
+
+    fun PerformLoginAsAdmin()
+    {
+        PerformLogin("daniel", "1234qwer")
+    }
+
+    @Test
+    fun PerformLogoutTest()
+    {
+        PerformLogout()
+    }
+
+    @Test
+    fun ButtonDeleteRecipeAdminIsVisible()
+    {
+        PerformLoginAsAdmin()
+        Thread.sleep(2000)
+        onView(withText("testi")).perform(scrollTo())
+        onView(withText("testi")).perform(click())
+        Thread.sleep(2000)
+
+        onView(withId(R.id.delete_recipe_button)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun ButtonDeleteRecipeIsNotVisible()
+    {
+        PerformLoginAsNonAdmin()
+        Thread.sleep(2000)
+        onView(withText("testi")).perform(scrollTo())
+        onView(withText("testi")).perform(click())
+        Thread.sleep(2000)
+
+        onView(withId(R.id.delete_recipe_button)).check(matches(not(isDisplayed())))
     }
 }
